a85256f6d0401e0965e55b7377813d0de8f0e1cf
Alice <alicewest.create@gmail.com>
2023-02-27 09:38:24 +0000

    package installed

---
+++ starter/index.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+console.log("hello")
+
+console.log(inquirer)
+
+// // array of questions for user
+// const questions = [
+
+// ];
+
+// // function to write README file
+// function writeToFile(data) {
+// }
+
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
