dc5f6268f321b2fb513f23845ced49a6bcaea0f3
Alice <alicewest.create@gmail.com>
2023-02-27 14:25:03 +0000

    license updated

--- starter/index.js
+++ starter/index.js
@@ -2,35 +2,106 @@ const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
-const { createDiffieHellmanGroup } = require("crypto");
 
 
 
-// // array of questions for user
-// const questions = [
-
+// array of questions for user
+// const questions = [ 
+// ["title", "Project Title:"], 
+// ["description", "Project Description"], 
+// ["installation" , "Installation Process"], 
+// ["usage", "Usage"], 
+// ["contributing", "Contributing"], 
+// ["test", "Test"], 
+// ["questions", "questions"],
 // ];
 
 
-
-inquirer
-  .prompt([
+inquirer.prompt([
     {
-      name: "title",
-      type: "input",
-      message: "Project title:",
+        name: "title",
+        type: "input",
+        message: "Project Title:",
     },
-])
-.then((answers) => {
 
+    {
+        name: "description",
+        type: "input",
+        message: "Project Description:",
+    },
+
+    {
+        name: "installation",
+        type: "input",
+        message: "Installation Process:",
+    },
+
+    {
+        name: "usage",
+        type: "input",
+        message: "Your Usage:",
+    },
+
+    {
+        name: "contributing",
+        type: "input",
+        message: "",
+    },
+
+    {
+        name: "test",
+        type: "input",
+        message: "Test",
+    },
+
+    {
+        name: "license",
+        type: "list",
+        message: "Select which license",
+        choices: ["License: MPL 2.0", "Documentation", "Website shields.io"]
+    },
+
+    {
+        name: "questions",
+        type: "input",
+        message: "Questions",
+    },
+
+]).then(answers => {
     console.log(answers);
-    const template = generateMarkdown(answers)
 
-    fs.writeFile(answers.title, template, (error) =>
+    const template = generateMarkdown(answers)
+    
+    fs.writeFile(`${answers.title}.md`, template, (error) =>
     error ? console.error(error) : console.log("Success")
     );
 
 })
+
+
+
+
+// questions.forEach(question => {
+//     console.log("Array of question",question);
+
+// inquirer
+//     .prompt(
+//         questions.forEach(question => {
+//             [{name: question[0], type: "input", message: question[1],},]
+//             } 
+//             )).then((answers) => {
+//             console.log(answers);
+//             const template = generateMarkdown(answers.question[0])
+
+//             // fs.writeFile(`${answers.title}.md`, template, (error) =>
+//             // error ? console.error(error) : console.log("Success")
+//             // );
+//     })
+
+
+// // })
+
+
 
 
 
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,10 +1,47 @@
 // function to generate markdown for README
+
+function getLicenseBadge(license){
+  switch (license) {
+    case "License: MPL 2.0":
+      const image = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+      return image
+    case "Documentation":
+        console.log("[![Documentation](https://img.shields.io/badge/GitBook-Docu-lightblue)](https://sulstice.gitbook.io/globalchem-your-chemical-graph-network/)");
+        return "Documentation" 
+    case "Website shields.io":
+        console.log("[![Website shields.io](https://img.shields.io/website-up-down-green-red/http/shields.io.svg)](http://www.chemicalgraphtheory.com)");
+        return "Website shields.io" 
+    }
+
+}
+
+
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title} \n
+## Description
+${data.description} \n
+
+## Installation
+${data.installation} \n
+
+## Usage
+${data.usage} \n
+
+## Contributing
+${data.contributing} \n
+
+## License
+${getLicenseBadge(data.license)} \n
+
+## Test
+${data.test} \n
+
+## Question
+${data.questions} \n
 
 `;
 }
 
-console.log("markdown");
-
 module.exports = generateMarkdown;
+
